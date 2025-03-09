8332de1bdc582a0d892a8513b87a2e33aecf0e53
Charalampos Vournazos <h.vournazos@hotmail.com>
2025-03-09 23:10:57 +0100

    working on with brainwave

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Hero from "./components/Hero.jsx";
+import Header from "./components/Header.jsx";
+import Benefits from "./components/Benefits.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
